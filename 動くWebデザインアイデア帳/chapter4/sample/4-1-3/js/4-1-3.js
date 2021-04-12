7528122c8a8d0a2356652a7de9b6fff98e9008d0
ssaaa <33023745+hanakata@users.noreply.github.com>
2021-04-12 16:53:42 +0900

    途中まで

---
+++ 動くWebデザインアイデア帳/chapter4/sample/4-1-3/js/4-1-3.js
@@ -0,0 +1,33 @@
+//テキストのカウントアップの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+    easing: 'easeInOut',
+    strokeWidth: 0.2,//進捗ゲージの太さ
+    duration: 1000,//時間指定(1000＝1秒)
+    trailWidth: 0.2,//線の太さ
+    color: '#555',
+    trailColor:'#bbb',
+    text: {//テキストの形状を直接指定 
+      style: {//天地中央に配置
+        position:'absolute',
+        left:'50%',
+        top:'50%',
+        padding:'0',
+        margin:'-30px 0 0 0',
+        transform:'translate(-50%,-50%)',
+        'font-size':'1rem',
+        color:'#fff',
+      },
+      autoStyleContainer: false //自動付与のスタイルを切る
+    },
+    step: function(state, bar) {
+      bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+    }
+  });
+  
+  //アニメーションスタート
+  bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+    $("#splash_text").fadeOut(10);
+    $(".loader_cover-up").addClass("coveranime");
+    $(".loader_cover-down").addClass("coveranime");
+    $("#splash").fadeOut();//アニメーションが終わったら#splashエリアをフェードアウト
+  }); 
