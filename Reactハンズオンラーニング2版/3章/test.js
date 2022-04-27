e19afd9523cfddf52bd2a12527d5a9e711025208
katayama.masatoshi <33023745+hanakata@users.noreply.github.com>
2022-04-27 11:42:57 +0900

    4章途中

---
+++ Reactハンズオンラーニング2版/3章/test.js
@@ -0,0 +1,10 @@
+var log = function(message){
+    console.log(message);
+};
+log("In JavaScript, function are variables")
+
+//アロー関数
+const log = message=>{
+    console.log(message);
+};
+log("In JavaScript, function are variables")
